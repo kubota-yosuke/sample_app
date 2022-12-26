0e8a123d255d28eaabdcf4dfeb50da10a64a49eb
kubota-yosuke <i.h.s.y.1491@gmail.com>
2022-12-26 19:08:52 +0900

    Userモデル作成とログイン機能実装

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#top'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users
+end
